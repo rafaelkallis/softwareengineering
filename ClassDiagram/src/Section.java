331bdfb8185f3db5f69fb21ecf355ff3cc8ac02a
fmonze <francimn_95@hotmail.com>
2015-10-23 16:54:32 +0200

    added section

--- ClassDiagram/src/Section.java
+++ ClassDiagram/src/Section.java
@@ -2,3 +2,124 @@
 public class Section {
 
 }
+
+// sections
+
+class WorldMapSection extends Section{
+	
+}
+class TableSection extends Section{
+	
+}
+class HeatMapSection extends Section{
+	
+}
+class StatisticsSection extends Section{
+	
+}
+class AdminareaSection extends Section{
+	
+}
+
+// views
+
+interface FilterViewInterface {
+}
+
+class RangeSliderView implements FilterViewInterface {
+  
+}
+class SelectListView implements FilterViewInterface {
+  
+}
+class LoginView {
+  
+}
+class ImportView {
+  
+}
+class PieChartView implements ChartViewInterface {
+  
+}
+class BarChartView implements ChartViewInterface {
+  
+}
+interface ChartViewInterface {
+  
+}
+class SearchBoxView implements FilterViewInterface {
+  
+}
+class AdvertismentView {
+  
+}
+class NavigationView {
+  
+}
+
+class WorldMapView{
+	
+}
+class TableView{
+	
+}
+class HeatMapView{
+	
+}
+class StatisticsView{
+	
+}
+class AdminareaView{
+	
+}
+
+// presenters
+
+class TablePresenter {
+
+}
+
+class LoginPresenter {
+
+}
+
+class ImportPresenter {
+
+}
+
+class StatisticsPresenter {
+
+}
+
+class ChartPresenter {
+	private ChartViewInterface view;
+}
+
+class FilterPresenter {
+	private FilterViewInterface view;
+	private MovieAttributeInterface attribute;
+}
+
+class YearFilterPresenter extends FilterPresenter {
+
+}
+
+class LanguageFilterPresenter extends FilterPresenter {
+
+}
+
+class GenreFilterPresenter extends FilterPresenter {
+
+}
+
+class CountryFilterPresenter extends FilterPresenter {
+
+}
+
+class AdvertisementPresenter {
+
+}
+
+class NavigationPresenter {
+
+}
