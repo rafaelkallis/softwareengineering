7f39669e250f63a25ee8f8d3d5b1e8d44127eaa0
fmonze <francimn_95@hotmail.com>
2015-10-23 16:01:53 +0200

    francesca.java

---
+++ ClassDiagram/src/francesca.java
@@ -0,0 +1,27 @@
+import java.lang.Object;
+
+public class francesca extends Object {
+	
+	class movieAttribute {}
+	
+	class movieTitle {}
+	
+	class movieYear {}
+	
+	class movieMonth {}
+	
+	class movieDay {}
+	
+	class movieLanguage {}
+	
+	class movieCountry {}
+	
+	class movieRating {}
+	
+	class movieDuration {}
+	
+	class movieID {}
+	
+	class movie {}
+
+}
