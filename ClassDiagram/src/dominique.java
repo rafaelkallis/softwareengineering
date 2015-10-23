68711e287a309ebc3d4ca621e20cfabef46d37d3
Dominique Sandoz <dominique.sandoz@gmail.com>
2015-10-23 15:12:36 +0200

    Finished classes

---
+++ ClassDiagram/src/dominique.java
@@ -0,0 +1,33 @@
+interface FilterViewInterface {
+  
+}
+class RangeSliderView {
+  
+}
+class SelectListView {
+  
+}
+class LoginView {
+  
+}
+class ImportView {
+  
+}
+class PieChartView {
+  
+}
+class BarChartView {
+  
+}
+interface ChartViewInterface {
+  
+}
+class SearchBoxView {
+  
+}
+class AdvertismentView {
+  
+}
+class NavigationView {
+  
+}
