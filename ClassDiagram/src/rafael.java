e7f5094cdec4d4eeb62709d46530314c83e3168a
fmonze <francimn_95@hotmail.com>
2015-10-23 16:54:53 +0200

    remove old classes

--- ClassDiagram/src/rafael.java
+++ ClassDiagram/src/rafael.java
@@ -1,42 +1,7 @@
+
 public class rafael {
 	
 }
 final class AppController{
-	
-}
-
-class Section{
-	
-}
-class WorldMapSection extends Section{
-	
-}
-class TableSection extends Section{
-	
-}
-class HeatMapSection extends Section{
-	
-}
-class StatisticsSection extends Section{
-	
-}
-class AdminareaSection extends Section{
-	
-}
-
-
-class WorldMapView{
-	
-}
-class TableView{
-	
-}
-class HeatMapView{
-	
-}
-class StatisticsView{
-	
-}
-class AdminareaView{
 	
 }
