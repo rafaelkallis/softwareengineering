62460e86251f858ee076df574a3eaa48cf35473d
Rafael Kallis <rafael@Rafaels-Mac.local>
2015-10-23 19:11:46 +0200

    added Shared.java

---
+++ ClassDiagram/src/Shared.java
@@ -0,0 +1,71 @@
+
+public class Shared {
+
+}
+
+final class AppController{
+	
+}
+
+abstract class MovieAttribute {
+}
+
+class MovieTitle extends MovieAttribute {}
+
+class MovieYear extends MovieAttribute {}
+
+class MovieMonth extends MovieAttribute{}
+
+class MovieDay extends MovieAttribute{}
+
+class MovieLanguage extends MovieAttribute {}
+
+class MovieCountry extends MovieAttribute {}
+
+class MovieRating extends MovieAttribute {}
+
+class MovieDuration extends MovieAttribute {}
+
+class MovieID extends MovieAttribute {}
+
+class Movie {
+	public MovieID id;
+	public MovieTitle title;
+	public MovieYear year;
+	public MovieLanguage[] languages;
+	public MovieCountry[] countries;
+	public MovieRating rating;
+	public MovieDuration duration;
+}
+
+class ImportRequest{
+	
+}
+class ImportResponse{
+	
+}
+class ExportRequest{
+	
+}
+class ExportResponse{
+}
+
+class WorldStatisticsModel{
+	
+}
+class StatisticsModel{
+	
+}
+class FilterModel{
+	
+}
+class MovieCollection{
+	
+}
+class User{
+	
+}
+class Adminuser{
+	
+}
+
