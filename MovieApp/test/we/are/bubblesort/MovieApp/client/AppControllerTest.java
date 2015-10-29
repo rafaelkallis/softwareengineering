6a7f6277e5b5cefe2c9d31d8d86e504981a992b5
Dominique Sandoz <dominique.sandoz@gmail.com>
2015-10-29 22:09:56 +0100

    First Test added and jUnit as well

---
+++ MovieApp/test/we/are/bubblesort/MovieApp/client/AppControllerTest.java
@@ -0,0 +1,13 @@
+package we.are.bubblesort.MovieApp.client;
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class AppControllerTest extends GWTTestCase {
+
+	public String getModuleName() {
+		return "we.are.bubblesort.MovieApp";
+	}
+	
+	public void testSimple() {
+		assertTrue(true);
+	}
+}
