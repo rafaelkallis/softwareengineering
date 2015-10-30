e9836743a55ba639dfb600edfcf9c63cdb833ea6
Rafael Kallis <rafael@Rafaels-Mac.local>
2015-10-30 20:58:13 +0100

    Improved MovieApp

--- MovieApp/src/we/are/bubblesort/MovieApp/client/MovieApp.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/client/MovieApp.java
@@ -4,14 +4,11 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.RootPanel;
 
-import we.are.bubblesort.MovieApp.shared.EventBus;
-
 public class MovieApp implements EntryPoint {
 	
   public void onModuleLoad() {
 	  QueryServiceAsync rpcService = GWT.create(QueryService.class);
-	  EventBus eventBus = new EventBus();
-	  AppController appViewer = new AppController(rpcService, eventBus);
+	  AppController appViewer = new AppController(rpcService);
 	  appViewer.init(RootPanel.get());
   }
 }
