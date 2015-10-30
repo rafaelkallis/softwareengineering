7a0e84cc26c021094340ff0461c32ac3f43e4d8b
Rafael Kallis <rafael@Rafaels-Mac.local>
2015-10-30 20:55:29 +0100

    Improved Collection

--- MovieApp/src/we/are/bubblesort/MovieApp/shared/Collection.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/shared/Collection.java
@@ -3,7 +3,8 @@ package we.are.bubblesort.MovieApp.shared;
 import java.util.Iterator;
 import java.util.List;
 
-public class Collection<T> extends EventBus implements Iterable<T> {
+
+public class Collection<T> extends Model implements Iterable<T> {
 	protected List<T> elements;
 	
 	public void add(T element) {
