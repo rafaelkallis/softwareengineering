9181e0aac6cf367d44e7a3fbad285c7df69b1d03
Rafael Kallis <rafael@Rafaels-Mac.local>
2015-10-30 20:52:36 +0100

    Added Model

--- MovieApp/src/we/are/bubblesort/MovieApp/shared/Model.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/shared/Model.java
@@ -1,6 +1,6 @@
 package we.are.bubblesort.MovieApp.shared;
-
 import com.google.web.bindery.event.shared.SimpleEventBus;
 
-public class EventBus extends SimpleEventBus {
+public abstract class Model extends SimpleEventBus{
+
 }
