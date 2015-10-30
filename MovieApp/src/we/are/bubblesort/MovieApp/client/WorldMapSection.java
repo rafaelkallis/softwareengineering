667459523dfae3ab18534dc9103fccbb78802a5d
Rafael Kallis <rafael@Rafaels-Mac.local>
2015-10-30 20:56:11 +0100

    Optimised WorldMapSection

--- MovieApp/src/we/are/bubblesort/MovieApp/client/WorldMapSection.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/client/WorldMapSection.java
@@ -1,19 +1,20 @@
 package we.are.bubblesort.MovieApp.client;
 
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.web.bindery.event.shared.SimpleEventBus;
+
 import com.google.gwt.user.client.ui.Panel;
 
-import we.are.bubblesort.MovieApp.shared.EventBus;
 
 public class WorldMapSection extends Section {
 	static final String defaultName = "Karte";
 	QueryServiceAsync queryService;
 	
-	WorldMapSection(String sectionName, Boolean showInMainMenu, EventBus globalEventBus, QueryServiceAsync queryService) {
+	WorldMapSection(String sectionName, Boolean showInMainMenu, SimpleEventBus globalEventBus, QueryServiceAsync queryService) {
 		super(sectionName, showInMainMenu, globalEventBus);
 	}
 	
-	WorldMapSection(Boolean showInMainMenu, EventBus globalEventBus, QueryServiceAsync queryService) {
+	WorldMapSection(Boolean showInMainMenu, SimpleEventBus globalEventBus, QueryServiceAsync queryService) {
 		this(defaultName, showInMainMenu, globalEventBus, queryService);
 	}
 
