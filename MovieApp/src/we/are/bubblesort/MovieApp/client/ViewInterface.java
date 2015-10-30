d857d463b1a022dd070a3156636cca9c852f71a9
Rafael Kallis <rafael@Rafaels-Mac.local>
2015-10-30 20:54:23 +0100

    Removed ViewInterface

--- MovieApp/src/we/are/bubblesort/MovieApp/client/ViewInterface.java
+++
@@ -1,9 +0,0 @@
-package we.are.bubblesort.MovieApp.client;
-
-import com.google.web.bindery.event.shared.Event;
-import com.google.web.bindery.event.shared.HandlerRegistration;
-
-import we.are.bubblesort.MovieApp.shared.Eventable;
-
-public interface ViewInterface extends Eventable{
-}
