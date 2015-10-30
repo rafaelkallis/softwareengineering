80831fb840328667d68616a17cae90d743925344
Rafael Kallis <rafael@Rafaels-Mac.local>
2015-10-30 20:53:52 +0100

    Optimised View

--- MovieApp/src/we/are/bubblesort/MovieApp/client/View.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/client/View.java
@@ -1,18 +1,11 @@
 package we.are.bubblesort.MovieApp.client;
 
-import com.google.web.bindery.event.shared.Event;
-import com.google.web.bindery.event.shared.HandlerRegistration;
+import com.google.web.bindery.event.shared.SimpleEventBus;
 
-import we.are.bubblesort.MovieApp.shared.EventBus;
-
-public class View extends EventBus implements ViewInterface {
-	@Override
-	public <H>HandlerRegistration subscribeToEvent(Event.Type<H> type, H handler) {
-		return this.addHandler(type, handler);
-	}
-
-	@Override
-	public void fireEvent(Event<?> event) {
-		this.fireEvent(event);
-	}
+abstract public class View extends SimpleEventBus {
+	/*
+	 * implemented in super-class:
+	 * 		void fireEvent(Event<?> event);
+	 * 		<H>HandlerRegistration addHandler(Event.Type<H> type, H handler);
+	 */
 }
