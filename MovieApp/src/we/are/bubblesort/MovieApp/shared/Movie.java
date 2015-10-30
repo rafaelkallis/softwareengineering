8b8cc6948444e8e8f0d6d464060942ac431e312e
Rafael Kallis <rafael@dhcp-wlan-uzh-89-206-77-228.uzh.ch>
2015-10-30 18:23:00 +0100

    implemented SetAddInterface

--- MovieApp/src/we/are/bubblesort/MovieApp/shared/Movie.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/shared/Movie.java
@@ -1,25 +1,27 @@
 package we.are.bubblesort.MovieApp.shared;
 import java.util.Set;
 
-public class Movie {
-		Movie(	MovieID id,
-				MovieTitle title,
-				MovieYear year,
-				Set<MovieLanguage> languages,
-				Set<MovieCountry> countries,
-				MovieDuration duration){
-			this.id = id;
-			this.title= title;
-			this.year = year;
-			this.languages = languages;
-			this.countries = countries;
-			this.duration = duration;
-		}
-
-		public MovieID id;
-		public MovieTitle title;
-		public MovieYear year;
-		public Set<MovieLanguage> languages;
-		public Set<MovieCountry> countries;
-		public MovieDuration duration;
+public class Movie implements SetAddInterface{
+	public MovieID id;
+	public MovieTitle title;
+	public MovieYear year;
+	public Set<MovieLanguage> languages;
+	public Set<MovieCountry> countries;
+	public MovieDuration duration;
+	Movie(	MovieID id,
+			MovieTitle title,
+			MovieYear year,
+			Set<MovieLanguage> languages,
+			Set<MovieCountry> countries,
+			MovieDuration duration){
+		this.id = id;
+		this.title= title;
+		this.year = year;
+		this.languages = languages;
+		this.countries = countries;
+		this.duration = duration;
+	}
+	public Object getKey(){
+		return this.id.value;
+	}
 }
