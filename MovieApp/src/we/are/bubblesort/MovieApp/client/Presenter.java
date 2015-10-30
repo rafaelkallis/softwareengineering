4204bc08854ecbf5e3a4e9a773fa1ec5cc6cd44b
Rafael Kallis <rafael@Rafaels-Mac.local>
2015-10-30 20:53:33 +0100

    Fixed Presenter implementation

--- MovieApp/src/we/are/bubblesort/MovieApp/client/Presenter.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/client/Presenter.java
@@ -1,10 +1,11 @@
 package we.are.bubblesort.MovieApp.client;
 
-import com.google.web.bindery.event.shared.Event;
-import com.google.web.bindery.event.shared.HandlerRegistration;
+import com.google.web.bindery.event.shared.SimpleEventBus;
 
-import we.are.bubblesort.MovieApp.shared.EventBus;
-import we.are.bubblesort.MovieApp.shared.Eventable;
-
-public class Presenter {
+public class Presenter extends SimpleEventBus{
+	/*
+	 * implemented in super-class:
+	 * 		void fireEvent(Event<?> event);
+	 * 		<H>HandlerRegistration addHandler(Event.Type<H> type, H handler);
+	 */
 }
