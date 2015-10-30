bfe44ec4fd749fbd65ddfba124d796560bb818b4
Rafael Kallis <rafael@Rafaels-Mac.local>
2015-10-30 20:51:23 +0100

    Added QueryService and QueryServiceAsync

--- MovieApp/src/we/are/bubblesort/MovieApp/client/QueryService.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/client/QueryService.java
@@ -1,11 +1,16 @@
 package we.are.bubblesort.MovieApp.client;
 
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+import we.are.bubblesort.MovieApp.shared.Collection;
+import we.are.bubblesort.MovieApp.shared.Movie;
 
 /**
  * The client-side stub for the RPC service.
  */
 @RemoteServiceRelativePath("query")
 public interface QueryService extends RemoteService {
+	public void getMovieSet(String database_name, AsyncCallback<Collection<Movie>> callback);
+	
 }
--- MovieApp/src/we/are/bubblesort/MovieApp/client/QueryServiceAsync.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/client/QueryServiceAsync.java
@@ -2,8 +2,13 @@ package we.are.bubblesort.MovieApp.client;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
+import we.are.bubblesort.MovieApp.shared.Collection;
+import we.are.bubblesort.MovieApp.shared.Movie;
+
 /**
  * The async counterpart of <code>QueryService</code>.
  */
 public interface QueryServiceAsync {
+
+	void getMovieSet(String database_name, AsyncCallback<Collection<Movie>> callback, AsyncCallback<Void> callback2);
 }
