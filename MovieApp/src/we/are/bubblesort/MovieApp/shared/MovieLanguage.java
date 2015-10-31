0c2e5c61835e46f7513445d4deb4c7eb977a79e2
Rafael Kallis <rafael@rafaelkallis.com>
2015-10-31 21:44:11 +0100

    added Static database name

--- MovieApp/src/we/are/bubblesort/MovieApp/shared/MovieLanguage.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/shared/MovieLanguage.java
@@ -1,7 +1,8 @@
 package we.are.bubblesort.MovieApp.shared;
 
 public class MovieLanguage extends MovieAttribute {
+	public static String dbName = "movie_languages";
 	MovieLanguage(String value){
-		super(value,"movie_languages");
+		super(value,MovieLanguage.dbName);
 	}
 }
