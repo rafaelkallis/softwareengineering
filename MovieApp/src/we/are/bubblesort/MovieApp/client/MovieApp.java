5b7d85aa733367b46eeb454deefe0b4a72c4a222
Rafael Kallis <rafael@rafaelkallis.com>
2015-11-01 12:25:04 +0100

    specified service url

--- MovieApp/src/we/are/bubblesort/MovieApp/client/MovieApp.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/client/MovieApp.java
@@ -1,6 +1,7 @@
 package we.are.bubblesort.MovieApp.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.rpc.ServiceDefTarget;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.RootPanel;
 
@@ -8,6 +9,8 @@ public class MovieApp implements EntryPoint {
 	
   public void onModuleLoad() {
 	  QueryServiceAsync queryService = GWT.create(QueryService.class);
+	  ((ServiceDefTarget) queryService).setServiceEntryPoint(GWT.getModuleBaseURL() + "/Query/QueryService");
+	  
 	  AppController appViewer = new AppController(queryService);
 	  appViewer.init(RootPanel.get());
   }
