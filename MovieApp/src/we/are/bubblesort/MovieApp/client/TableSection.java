be6147d8eb6a7698c345a6ede2ba9960a391bb72
Rafael Kallis <rafael@rafaelkallis.com>
2015-11-01 12:25:24 +0100

    added callQueryService

--- MovieApp/src/we/are/bubblesort/MovieApp/client/TableSection.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/client/TableSection.java
@@ -1,7 +1,13 @@
 package we.are.bubblesort.MovieApp.client;
 
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.web.bindery.event.shared.SimpleEventBus;
+
+import we.are.bubblesort.MovieApp.shared.Collection;
+import we.are.bubblesort.MovieApp.shared.Movie;
+import we.are.bubblesort.MovieApp.shared.MovieAttribute;
+import we.are.bubblesort.MovieApp.shared.Set;
 
 public class TableSection extends Section {
 	static final String defaultName = "Tabelle";
@@ -19,5 +25,19 @@ public class TableSection extends Section {
 	@Override
 	void init() {
 		this.mainPanel = new FlowPanel();
+	}
+	public void callQueryService(Set<MovieAttribute> filterSet){
+		queryService.getMovieCollection(filterSet, new AsyncCallback<Collection<Movie>>(){
+			public void onFailure(Throwable caught){
+				/*
+				 * handle Failure
+				 */
+			}
+			public void onSuccess(Collection<Movie> result){
+				/*
+				 * to something with result
+				 */
+			}
+		});
 	}
 }
--- MovieApp/src/we/are/bubblesort/MovieApp/client/WorldMapSection.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/client/WorldMapSection.java
@@ -1,7 +1,13 @@
 package we.are.bubblesort.MovieApp.client;
 
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.web.bindery.event.shared.SimpleEventBus;
+
+import we.are.bubblesort.MovieApp.shared.Collection;
+import we.are.bubblesort.MovieApp.shared.Movie;
+import we.are.bubblesort.MovieApp.shared.MovieAttribute;
+import we.are.bubblesort.MovieApp.shared.Set;
 
 import com.google.gwt.user.client.ui.Panel;
 
@@ -12,6 +18,7 @@ public class WorldMapSection extends Section {
 	
 	WorldMapSection(String sectionName, Boolean showInMainMenu, SimpleEventBus globalEventBus, QueryServiceAsync queryService) {
 		super(sectionName, showInMainMenu, globalEventBus);
+		this.queryService = queryService;
 	}
 	
 	WorldMapSection(Boolean showInMainMenu, SimpleEventBus globalEventBus, QueryServiceAsync queryService) {
@@ -22,5 +29,18 @@ public class WorldMapSection extends Section {
 	void init() {
 		this.mainPanel = new FlowPanel();
 	}
-	
+	public void callQueryService(Set<MovieAttribute> filterSet){
+		queryService.getMovieCollection(filterSet, new AsyncCallback<Collection<Movie>>(){
+			public void onFailure(Throwable caught){
+				/*
+				 * handle Failure
+				 */
+			}
+			public void onSuccess(Collection<Movie> result){
+				/*
+				 * to something with result
+				 */
+			}
+		});
+	}
 }
