5f0f85f0ce22405c4383c733ff6cc5169ab73e0d
Rafael Kallis <rafael@rafaelkallis.com>
2015-11-02 15:24:25 +0100

    Fixes

--- MovieApp/src/we/are/bubblesort/MovieApp/shared/MovieAttribute.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/shared/MovieAttribute.java
@@ -1,19 +1,21 @@
 package we.are.bubblesort.MovieApp.shared;
 
-import com.google.gwt.user.client.rpc.IsSerializable;
+public abstract class MovieAttribute implements java.io.Serializable,Comparable<MovieAttribute>{
 
-public abstract class MovieAttribute implements IsSerializable,Comparable<Object>{
-	public Object value;
+	private static final long serialVersionUID = 7986627264620732095L;
+	public java.io.Serializable value;
 	public String displayName;
 	public String dbLabelName;
 	
-	public MovieAttribute(Object value,String dbLabelName,String displayName){
+	public MovieAttribute(java.io.Serializable value,String dbLabelName,String displayName){
 		this.value = value;
 		this.dbLabelName = dbLabelName;
 		this.displayName = displayName;
 	}
-	public int compareTo(Object o){
-		return value.toString().compareTo(o.toString());
+	
+	@Override
+	public int compareTo(MovieAttribute a){
+		return value.toString().compareTo(a.toString());
 	}
 	
 	@Override
