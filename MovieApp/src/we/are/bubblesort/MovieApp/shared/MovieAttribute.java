57a38a95a883c0f6459e9bd10432340da33e5583
Rafael Kallis <rafael@rafaelkallis.com>
2015-11-02 11:27:17 +0100

    made MovieAttributes Comparable, removed keyable

--- MovieApp/src/we/are/bubblesort/MovieApp/shared/MovieAttribute.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/shared/MovieAttribute.java
@@ -2,14 +2,20 @@ package we.are.bubblesort.MovieApp.shared;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
-public abstract class MovieAttribute implements Keyable, IsSerializable{
+public abstract class MovieAttribute implements IsSerializable,Comparable<Object>{
 	public Object value;
 	public String dbLabelName;
 	public MovieAttribute(Object value,String dbLabelName){
 		this.value = value;
 		this.dbLabelName = dbLabelName;
 	}
-	public Object getKey(){
-		return this.value;
+
+	public int compareTo(Object o){
+		return value.toString().compareTo(o.toString());
+	}
+	
+	@Override
+	public int hashCode(){
+		return value.hashCode();
 	}
 }
