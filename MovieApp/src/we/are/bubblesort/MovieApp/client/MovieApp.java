cc20a96790b8d8b87bf92cb25cfe5450044191f7
Rafael Kallis <rafael@rafaelkallis.com>
2015-11-02 16:02:31 +0100

    button added for testing

--- MovieApp/src/we/are/bubblesort/MovieApp/client/MovieApp.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/client/MovieApp.java
@@ -1,8 +1,12 @@
 package we.are.bubblesort.MovieApp.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class MovieApp implements EntryPoint {
@@ -13,5 +17,16 @@ public class MovieApp implements EntryPoint {
 	  
 	  AppController appViewer = new AppController(queryService);
 	  appViewer.init(RootPanel.get());
+	  
+	  Button dbTestButton = new Button("Test DB Call");
+		dbTestButton.addStyleName("TEST-UI");
+		dbTestButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				// do some shit and log it out
+				Window.alert("DB TEST!");
+			}
+		});
+	  RootPanel.get().add(dbTestButton);
   }
 }
