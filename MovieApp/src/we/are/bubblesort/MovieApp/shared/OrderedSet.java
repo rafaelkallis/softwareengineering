e68d86c6623ae5d6e44afe76e3623e34fac22108
Rafael Kallis <rafael@rafaelkallis.com>
2015-11-02 15:25:57 +0100

    improved OrderedSet

--- MovieApp/src/we/are/bubblesort/MovieApp/shared/OrderedSet.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/shared/OrderedSet.java
@@ -1,33 +1,19 @@
 package we.are.bubblesort.MovieApp.shared;
 
-import java.util.Iterator;
-import java.util.TreeSet;
-
-public class OrderedSet <T extends Comparable<Object>> implements Iterable<T>{
-	protected TreeSet<T> elements;
+public class OrderedSet <T extends Comparable<T>> implements java.io.Serializable,Iterable<T>{
+	
+	private static final long serialVersionUID = 5907640708328632853L;
+	protected java.util.TreeSet<T> elements;
 	public OrderedSet(){
-		elements = new TreeSet<T>();
+		elements = new java.util.TreeSet<T>();
 	}
+	
 	public void add(T element){
 		elements.add(element);
 	}
-	
-	public void addAll(OrderedSet<T> collection_to_import){
-		this.elements.addAll(collection_to_import.elements);
-	}
-	public void addAll(Collection<T> collection_to_import){
-		for(T element : collection_to_import){
-			this.elements.add(element);
-		}
-	}
-	public void addAll(UnorderedSet<T> collection_to_import){
-		for(T element : collection_to_import){
-			this.elements.add(element);
-		}
-	}
 
 	@Override
-	public Iterator<T> iterator(){
+	public java.util.Iterator<T> iterator(){
 		return elements.iterator();
 	}
 
