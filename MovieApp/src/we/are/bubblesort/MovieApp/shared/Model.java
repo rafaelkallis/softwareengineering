69dd87c31f50c8cc2cdda969c1b5b62042924546
Rafael Kallis <rafael@rafaelkallis.com>
2015-11-02 15:52:23 +0100

    Implemented SimpleEventBus instance

--- MovieApp/src/we/are/bubblesort/MovieApp/shared/Model.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/shared/Model.java
@@ -1,6 +1,22 @@
 package we.are.bubblesort.MovieApp.shared;
 import com.google.web.bindery.event.shared.SimpleEventBus;
+import com.google.web.bindery.event.shared.Event.Type;
+import com.google.web.bindery.event.shared.Event;
+import com.google.web.bindery.event.shared.HandlerRegistration;
 
-public abstract class Model extends SimpleEventBus{
 
+public abstract class Model{
+	SimpleEventBus eventBus;
+	
+	public Model(){
+		eventBus = new SimpleEventBus();
+	}
+	
+	public void fireEvent(Event<?> e){
+		this.eventBus.fireEvent(e);
+	}
+	
+	public <H> HandlerRegistration addHandler(Type<H> type, H handler){
+		return this.eventBus.addHandler(type, handler);
+	}
 }
