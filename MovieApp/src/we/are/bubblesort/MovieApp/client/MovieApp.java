50dca01c4f39f6f93e35037c848cef4044140d56
Rafael Kallis <rafael@rafaelkallis.com>
2015-11-03 22:40:52 +0100

    removed tests

--- MovieApp/src/we/are/bubblesort/MovieApp/client/MovieApp.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/client/MovieApp.java
@@ -1,14 +1,9 @@
 package we.are.bubblesort.MovieApp.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.Button;
+
 import com.google.gwt.user.client.ui.RootPanel;
-import we.are.bubblesort.MovieApp.shared.*;
 
 public class MovieApp implements EntryPoint {
 	  QueryServiceAsync queryService = GWT.create(QueryService.class);
@@ -16,30 +11,6 @@ public class MovieApp implements EntryPoint {
   public void onModuleLoad() {
 	  
 	  AppController appViewer = new AppController(queryService);
-	  appViewer.init(RootPanel.get());
-	  
-	  Button dbTestButton = new Button("Test DB Call");
-		dbTestButton.addStyleName("TEST-UI");
-		dbTestButton.addClickHandler(new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				// do some shit and log it out
-				filter_test();
-			}
-		});
-	  RootPanel.get().add(dbTestButton);
-  }
-  public void filter_test(){
-	  queryService.getAttributeSet(new MovieYear(1994,"1994"),1,0, new AsyncCallback<OrderedSet<MovieAttribute>>(){
-		  public void onSuccess(OrderedSet<MovieAttribute> result){
-			  for(MovieAttribute atr : result){
-				  Window.alert("Returned: "+atr.value);
-			  }
-			  
-		  }
-		  public void onFailure(Throwable e){
-			  Window.alert(":(");
-		  }
-	  });
+	  appViewer.init(RootPanel.get());  
   }
 }
