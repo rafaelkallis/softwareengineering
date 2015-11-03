998dbdffee186c6b94e7bb9584d900bbc9757967
Rafael Kallis <rafael@rafaelkallis.com>
2015-11-03 23:16:17 +0100

    Made Threadsafe

--- MovieApp/src/we/are/bubblesort/MovieApp/server/Database.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/server/Database.java
@@ -8,23 +8,28 @@ import java.sql.SQLException;
 
 public final class Database {
 		
-	private static Database instance = null;
+	private static Database instance = new Database();
 	
 	private  		Connection 	conn 		= null;
-    private  final 	String 		table_name 	= "movies";
     private  final 	String 		url  		= "jdbc:mysql://80.74.150.210:3306/movieapp";
     private  final 	String 		user 		= "se_user";
     private  final 	String 		pass 		= "SEIsAwesome2015";
     
-    private Database() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
-    	Class.forName("com.mysql.jdbc.Driver").newInstance();
-        conn = DriverManager.getConnection(url, user, pass);
+    private Database(){
+    	try {
+    		
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			conn = DriverManager.getConnection(url, user, pass);
+			
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
     }
     
-    public static Database getInstance() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException{
-    	if(instance == null){
-    		instance = new Database();
-    	}
+    /*
+     * @returns Database instance of Database
+     */
+    public static Database getInstance() {
     	return instance;
     }
 	
@@ -36,20 +41,12 @@ public final class Database {
 		return pst.executeQuery();
 	}
 	
-	
 	/*
 	 * @param sql string used as a command
 	 * @returns PreparedStatement
 	 */
 	public PreparedStatement prepareStatement(String sql) throws SQLException{
 		return conn.prepareStatement(sql);
-	}
-	
-	/*
-	 * @returns table name
-	 */
-	public String get_table_name(){
-		return table_name;
 	}
 	
 }
