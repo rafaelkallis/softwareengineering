2ad5ca5bbfc827d78a3c9a622fe0940ef7c039e0
Rafael Kallis <rafael@rafaelkallis.com>
2015-11-03 15:27:20 +0100

    Made value String

--- MovieApp/src/we/are/bubblesort/MovieApp/shared/MovieAttribute.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/shared/MovieAttribute.java
@@ -1,14 +1,15 @@
 package we.are.bubblesort.MovieApp.shared;
 
-public abstract class MovieAttribute implements java.io.Serializable,Comparable<MovieAttribute>{
+import com.google.gwt.user.client.rpc.IsSerializable;
 
-	private static final long serialVersionUID = 7986627264620732095L;
-	public java.io.Serializable value;
+public abstract class MovieAttribute implements IsSerializable,Comparable<MovieAttribute>{
+
+	public String value;
 	public String displayName;
 	public String dbLabelName;
 	
 	public MovieAttribute(java.io.Serializable value,String dbLabelName,String displayName){
-		this.value = value;
+		this.value = value.toString();
 		this.dbLabelName = dbLabelName;
 		this.displayName = displayName;
 	}
