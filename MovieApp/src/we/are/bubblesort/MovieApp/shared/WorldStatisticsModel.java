d788c3aea3ed7d12595693abc5a481a89f3206d9
Rafael Kallis <rafael@rafaelkallis.com>
2015-11-05 17:12:30 +0100

    Removed WorldStatisticsModelEntry

--- MovieApp/src/we/are/bubblesort/MovieApp/shared/WorldStatisticsModel.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/shared/WorldStatisticsModel.java
@@ -2,51 +2,26 @@ package we.are.bubblesort.MovieApp.shared;
 
 import java.util.Iterator;
 import com.google.gwt.user.client.rpc.IsSerializable;
+import we.are.bubblesort.MovieApp.shared.WorldStatisticsModelEntry;
 
-public class WorldStatisticsModel extends Model implements IsSerializable,Iterable<WorldStatisticsModel.WorldStatisticsModelEntry>{
+public class WorldStatisticsModel extends Model implements IsSerializable,Iterable<WorldStatisticsModelEntry>{
 	
 	/*
 	 * Implements ordered set, so can be iterated in-order
 	 */
 	public OrderedSet<WorldStatisticsModelEntry> model = new OrderedSet<WorldStatisticsModelEntry>();
 	
+	/*
+	 * Database Labels
+	 */
+	public static final String	iso_alpha_DbLabelName 	= "iso3166-1-alpha-2";
+	public static final String 	iso_numeric_DbLabelName = "iso3166-1-numeric";
+	public static final String 	n_movies_DbLabelName 	= "NumberOfMovies";
 
 	public WorldStatisticsModel(){
 		super();
 	}
-	
-	public class WorldStatisticsModelEntry implements IsSerializable,Comparable<WorldStatisticsModelEntry>{
-		
-		/*
-		 * Values of each entry
-		 */
-		String 						iso_alpha;
-		Integer 					iso_numeric;
-		Integer 					n_movies;
-		
-		/*
-		 * Database Labels
-		 */
-		public static final String	iso_alpha_DbLabelName 	= "iso3166-1-alpha-2";
-		public static final String 	iso_numeric_DbLabelName = "iso3166-1-numeric";
-		public static final String 	n_movies_DbLabelName 	= "NumberOfMovies";
-		
-		public WorldStatisticsModelEntry(){
-			
-		}
-		
-		public WorldStatisticsModelEntry(String iso_alpha,Integer iso_numeric,Integer n_movies){
-			this.iso_alpha 		= iso_alpha;
-			this.iso_numeric 	= iso_numeric;
-			this.n_movies 		= n_movies;
-		}
-		
-		@Override
-		public int compareTo(WorldStatisticsModelEntry e){
-			return iso_alpha.compareTo(e.iso_alpha);
-		}
-	}
-	
+
 	public void add(WorldStatisticsModelEntry new_entry){
 		model.add(new_entry);
 	}
@@ -56,7 +31,7 @@ public class WorldStatisticsModel extends Model implements IsSerializable,Iterab
 	 * @see java.lang.Iterable#iterator()
 	 */
 	@Override
-	public Iterator<WorldStatisticsModel.WorldStatisticsModelEntry> iterator() {
+	public Iterator<WorldStatisticsModelEntry> iterator() {
 		return model.iterator();
 	}
 }
