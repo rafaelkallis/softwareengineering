4e3158c5b112e5ef0fbd4f327acd11659083bca0
Rafael Kallis <rafael@rafaelkallis.com>
2015-11-06 13:36:09 +0100

    Improved Database JUnit test case

--- MovieApp/test/we/are/bubblesort/MovieApp/server/DatabaseTest.java
+++ MovieApp/test/we/are/bubblesort/MovieApp/server/DatabaseTest.java
@@ -1,11 +1,8 @@
 package we.are.bubblesort.MovieApp.server;
 
 import org.junit.Test;
-
 import we.are.bubblesort.MovieApp.server.Database;
-import we.are.bubblesort.MovieApp.shared.*;
 import static org.junit.Assert.*;
-
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,15 +10,33 @@ import java.sql.SQLException;
 
 public class DatabaseTest {
 	
+	protected Database db;
+	final protected String testQuery = "SELECT * FROM `movieapp`.`movies`;";
 
 	@Test
-	public void test_database() throws SQLException{
-		Database db			= Database.getInstance();
+	public void test_database_instance() throws SQLException{
+		db						= Database.getInstance();
+		
 		assertNotNull(db);
+	}
+	
+	
+	@Test
+	public void test_database_prepareStatements() throws SQLException{
+		 db						= Database.getInstance();
+		PreparedStatement pst 	= db.prepareStatement(testQuery);
+		
+		assertNotNull(pst);
+	}
+	
+	@Test 
+	public void test_database_execute() throws SQLException{
+		db						= Database.getInstance();
 		
 		ResultSet rs;
 		
-		rs = db.execute(db.prepareStatement("SELECT * FROM `movieapp`.`movies`;"));
+		rs 						= db.execute(db.prepareStatement(testQuery));
+		
 		assertNotNull(rs);
 		assertTrue(rs.next());
 	}
