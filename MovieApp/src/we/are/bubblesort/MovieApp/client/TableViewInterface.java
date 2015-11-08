6d3dc8b27ea1717d2d95a8e408efa42d9d20ee32
Rafael Kallis <rafael@rafaelkallis.com>
2015-11-08 16:20:10 +0100

    added missing functions

--- MovieApp/src/we/are/bubblesort/MovieApp/client/TableViewInterface.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/client/TableViewInterface.java
@@ -2,7 +2,12 @@ package we.are.bubblesort.MovieApp.client;
 
 import java.util.ArrayList;
 
+import com.google.web.bindery.event.shared.Event.Type;
+
 public interface TableViewInterface {
 	public void setHeader(ArrayList<String> headers);
 	public void addItem(ArrayList<String> item);
+	public void addHandler(Type<LoadMoreEventHandler> tYPE, TablePresenter tablePresenter);
+	public void clearRows();
+	public int getRowCount();
 }
