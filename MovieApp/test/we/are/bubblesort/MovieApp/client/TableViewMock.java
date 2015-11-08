effb4308fe28a0bed47265e7599781a9fcc87bff
Rafael Kallis <rafael@rafaelkallis.com>
2015-11-08 15:22:37 +0100

    Corrected Mock

--- MovieApp/test/we/are/bubblesort/MovieApp/client/TableViewMock.java
+++ MovieApp/test/we/are/bubblesort/MovieApp/client/TableViewMock.java
@@ -1,7 +1,21 @@
 package we.are.bubblesort.MovieApp.client;
 
+import java.util.ArrayList;
+
 import we.are.bubblesort.MovieApp.client.TableView;
 
-public class TableViewMock extends TableView{
+public class TableViewMock implements TableViewInterface{
+
+	@Override
+	public void setHeader(ArrayList<String> headers) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addItem(ArrayList<String> item) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
