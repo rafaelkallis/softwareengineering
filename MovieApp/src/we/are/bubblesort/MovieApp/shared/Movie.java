de45238def924b59544c24cb6d77e8f23054b1ac
Rafael Kallis <rafael@rafaelkallis.com>
2015-11-12 09:56:59 +0100

    Added toJoinedString()

--- MovieApp/src/we/are/bubblesort/MovieApp/shared/Movie.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/shared/Movie.java
@@ -1,6 +1,10 @@
 package we.are.bubblesort.MovieApp.shared;
 
 import we.are.bubblesort.MovieApp.shared.UnorderedSet;
+import org.apache.commons.lang3.StringUtils;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.gwt.user.client.rpc.IsSerializable;
 
 public class Movie implements IsSerializable{
@@ -8,16 +12,10 @@ public class Movie implements IsSerializable{
 	public MovieTitle title;
 	public MovieYear year;
 	public MovieDuration duration;
-
-
-	public UnorderedSet<MovieLanguage> languages;
-	
-
+	public UnorderedSet<MovieLanguage> languages;	
 	public UnorderedSet<MovieCountry> countries;
-	
-
 	public UnorderedSet<MovieGenre> genres;
-	
+		
 	public Movie(){
 		
 	}
@@ -41,5 +39,19 @@ public class Movie implements IsSerializable{
 	@Override
 	public int hashCode(){
 		return id.hashCode();
+	}
+	
+	public String toJoinedString(String delimiter){
+		List<String> attribute_list = new ArrayList<String>(7);
+		
+		attribute_list.add(		id.toString());
+		attribute_list.add(		title.toString());
+		attribute_list.add(		year.toString());
+		attribute_list.add(		duration.toString());
+		attribute_list.add("\""+languages.toJoinedString(delimiter)	+"\"");
+		attribute_list.add("\""+countries.toJoinedString(delimiter)	+"\"");
+		attribute_list.add("\""+genres.toJoinedString(delimiter)		+"\"");
+		
+		return StringUtils.join(attribute_list, delimiter);
 	}
 }
