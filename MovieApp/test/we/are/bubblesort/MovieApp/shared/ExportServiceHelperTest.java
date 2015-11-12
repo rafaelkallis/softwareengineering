b58386c14496636aca6ee79750d94517b3dff5cc
Rafael Kallis <rafael@rafaelkallis.com>
2015-11-12 21:49:39 +0100

    refactored

--- MovieApp/test/we/are/bubblesort/MovieApp/shared/ExportServiceHelperTest.java
+++ MovieApp/test/we/are/bubblesort/MovieApp/shared/ExportServiceHelperTest.java
@@ -6,7 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ExportHelperTest {
+public class ExportServiceHelperTest {
 
 	@Before
 	public void setUp() throws Exception {
@@ -14,16 +14,6 @@ public class ExportHelperTest {
 
 	@After
 	public void tearDown() throws Exception {
-	}
-
-	@Test
-	public void testfilterSetToUrlString() throws UnsupportedEncodingException {
-		UnorderedSet<MovieAttribute> filterSet = new UnorderedSet<MovieAttribute>();
-		filterSet.add(new MovieTitle("Pulp Fiction"));
-		filterSet.add(new MovieYear("1994"));
-		filterSet.add(new MovieCountry("DE","Germany"));
-		
-		assertEquals("movie_countries=DE%7CGermany&movie_release_year=1994&movie_name=Pulp+Fiction",ExportServiceHelper.filterSetToUrl(filterSet));
 	}
 	
 	@Test
@@ -35,11 +25,6 @@ public class ExportHelperTest {
 		assertEquals("Schweiz",country_filter.displayName);
 	}
 	
-	@Test
-	public void testFilterToUrlString() throws UnsupportedEncodingException{
-		assertEquals("movie_name=Pulp+Fiction",ExportServiceHelper.filterToQueryString(new MovieTitle("Pulp Fiction")));
-		assertEquals("movie_countries=CH%7CSchweiz",ExportServiceHelper.filterToQueryString(new MovieCountry("CH","Schweiz")));
-	}
 	
 	@Test
 	public void testQueryStringToFilterSet() throws UnsupportedEncodingException{
@@ -48,7 +33,7 @@ public class ExportHelperTest {
 		compareSet.add(new MovieYear("1994"));
 		compareSet.add(new MovieCountry("DE","Germany"));
 		
-		UnorderedSet<MovieAttribute> filterSet = ExportServiceHelper.queryStringToFilterSet("movie_countries=DE|Germany&movie_release_year=1994&movie_name=Pulp Fiction");
+		UnorderedSet<MovieAttribute> filterSet = ExportServiceHelper.queryStringToFilterSet("movie_countries=DE~Germany&movie_release_year=1994&movie_name=Pulp Fiction");
 		
 		for(MovieAttribute filter: filterSet){
 			if(!filter.value.equals("Pulp Fiction")&& !filter.value.equals("1994")&& !filter.value.equals("DE")){
