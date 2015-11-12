0b8a403289b8dd6187a588284e72ab24fc445891
Rafael Kallis <rafael@rafaelkallis.com>
2015-11-12 20:58:55 +0100

    improved doGet()

--- MovieApp/src/we/are/bubblesort/MovieApp/server/ExportService.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/server/ExportService.java
@@ -17,34 +17,28 @@ public class ExportService extends HttpServlet {
 	
 	@Override
 	public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
-		try {
-			String queryString = request.getQueryString();
-			if(queryString == null){
-				response.sendError(400, "Empty Query");
-				return;
-			}		
-			UnorderedSet<MovieAttribute> filterSet = ExportServiceHelper.queryStringToFilterSet(queryString);
-			if(filterSet == null){
-				response.sendError(400, "Invalid Syntax" );
-				return;
-			}
-			
-			System.out.println(filterSet.toJoinedString(", "));
-			Collection<Movie> movies = QueryServiceImpl.getMovieCollectionSync(filterSet, 0, 0);
-			if(movies == null){
-				response.sendError(400, "No Movies found" );
-				return;
-			}
-			
-			response.setContentType("text/csv");
-			response.setHeader("Content-Disposition", "attachment; filename=\"MovieExport.csv\"");
-			OutputStream o = response.getOutputStream();
-			o.write(("id,Title,Year,Duration,Languages,Countries,Genres\n"+ExportServiceHelper.toSV(movies, ",")).getBytes());
-			o.flush();
-		} catch (Exception e) {
-			e.printStackTrace();
-			response.sendError(400, e.getMessage());
+		String queryString = request.getQueryString();
+		if(queryString == null){
+			response.sendError(400, "Empty Query");
+			return;
+		}	
+		
+		UnorderedSet<MovieAttribute> filterSet = ExportServiceHelper.queryStringToFilterSet(queryString);
+		if(filterSet.size() == 0){
+			response.sendError(400, "Invalid Syntax" );
+			return;
 		}
 		
+		Collection<Movie> movies = QueryServiceImpl.getMovieCollectionSync(filterSet, 0, 0);
+		if(movies.size() == 0){
+			response.sendError(400, "No Movies found" );
+			return;
+		}
+		
+		response.setContentType("text/csv");
+		response.setHeader("Content-Disposition", "attachment; filename=\"MovieExport.csv\"");
+		OutputStream o = response.getOutputStream();
+		o.write(("id,Title,Year,Duration,Languages,Countries,Genres\n"+ExportServiceHelper.toSV(movies, ",")).getBytes());
+		o.flush();
 	}
 }
