07a0c07dae08e21fc85c0edb2ddb8e6099b5fff7
Rafael Kallis <rafael@rafaelkallis.com>
2015-11-12 09:57:45 +0100

    added urlName

--- MovieApp/src/we/are/bubblesort/MovieApp/shared/MovieYear.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/shared/MovieYear.java
@@ -3,12 +3,13 @@ package we.are.bubblesort.MovieApp.shared;
 public class MovieYear extends MovieAttribute {
 	
 	public static final String dbLabelName = "movie_release_year";
+	public static final String urlName		= dbLabelName;
 	
 	public MovieYear(){
-		super(null,MovieYear.dbLabelName,null);
+		super(null,MovieYear.dbLabelName,MovieYear.urlName,null);
 	}
 	
 	public MovieYear(String value){
-		super(value,MovieYear.dbLabelName,value);
+		super(value,MovieYear.dbLabelName,MovieYear.urlName,value);
 	}
 }
