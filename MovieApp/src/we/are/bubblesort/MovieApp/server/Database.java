5cd0cfb0bfa3472967a75ae5ff92f26b6313f627
rafaelkallis <rafael@rafaelkallis.com>
2015-11-21 18:49:08 +0100

    removed execution functions

--- MovieApp/src/we/are/bubblesort/MovieApp/server/Database.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/server/Database.java
@@ -3,7 +3,6 @@ package we.are.bubblesort.MovieApp.server;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import com.google.appengine.api.utils.SystemProperty;
 
@@ -43,14 +42,6 @@ public final class Database {
     public static Database getInstance() {
     	return instance;
     }
-	
-    /*
-     * @param pst PreparedStatement to be executed
-     * @returns ResultSet 
-     */
-	public ResultSet execute(PreparedStatement pst) throws SQLException{
-		return pst.executeQuery();
-	}
 	
 	/*
 	 * @param sql string used as a command
