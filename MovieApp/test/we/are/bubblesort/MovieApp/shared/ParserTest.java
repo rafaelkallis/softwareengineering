4672a108879c285f04bea285f0c48712e47013d1
rafaelkallis <rafael@rafaelkallis.com>
2015-11-23 20:09:03 +0100

    resolved issues

--- MovieApp/test/we/are/bubblesort/MovieApp/shared/ParserTest.java
+++ MovieApp/test/we/are/bubblesort/MovieApp/shared/ParserTest.java
@@ -11,22 +11,22 @@ public class ParserTest {
 	@Test
 	public void testParse() throws IOException {
 		String tsv_string0 = "31138563	/m/0gh76d7	A Royal Divorce	1926			{}	{}	{\"/m/03g3w\": \"History\", \"/m/07s9rl0\": \"Drama\"}";
-		assertEquals("A Royal Divorce",					parser.parseTSV(tsv_string0)[0]);
-		assertEquals("1926",							parser.parseTSV(tsv_string0)[1]);
-		assertEquals("",								parser.parseTSV(tsv_string0)[2]);
-		assertEquals("",								parser.parseTSV(tsv_string0)[3]);
-		assertEquals("",								parser.parseTSV(tsv_string0)[4]);
-		assertEquals("",								parser.parseTSV(tsv_string0)[5]);
-		assertEquals("History, Drama",					parser.parseTSV(tsv_string0)[6]);
+		assertEquals("A Royal Divorce",					parser.parseTSV(tsv_string0)[0][0].toString());
+		assertEquals("1926",							parser.parseTSV(tsv_string0)[1][0].toString());
+		assertEquals("",								parser.parseTSV(tsv_string0)[2][0].toString());
+		assertEquals("",								parser.parseTSV(tsv_string0)[3][0].toString());
+		assertEquals("",								parser.parseTSV(tsv_string0)[4][0].toString());
+		assertEquals("",								parser.parseTSV(tsv_string0)[5][0].toString());
+		assertEquals("History, Drama",					parser.parseTSV(tsv_string0)[6][0].toString());
 		
 		String tsv_string1 = "9919728	/m/06_y9vs	Voy a hablar de la esperanza	1966			{\"/m/06nm1\": \"Spanish Language\"}	{\"/m/0jgd\": \"Argentina\"}	{\"/m/07s9rl0\": \"Drama\"}";
-		assertEquals("Voy a hablar de la esperanza",	parser.parseTSV(tsv_string1)[0]);
-		assertEquals("1966",							parser.parseTSV(tsv_string1)[1]);
-		assertEquals("",								parser.parseTSV(tsv_string1)[2]);
-		assertEquals("",								parser.parseTSV(tsv_string1)[3]);
-		assertEquals("Spanish Language",				parser.parseTSV(tsv_string1)[4]);
-		assertEquals("Argentina",						parser.parseTSV(tsv_string1)[5]);
-		assertEquals("Drama",							parser.parseTSV(tsv_string1)[6]);
+		assertEquals("Voy a hablar de la esperanza",	parser.parseTSV(tsv_string1)[0][0]);
+		assertEquals("1966",							parser.parseTSV(tsv_string1)[1][0]);
+		assertEquals("",								parser.parseTSV(tsv_string1)[2][0]);
+		assertEquals("",								parser.parseTSV(tsv_string1)[3][0]);
+		assertEquals("Spanish Language",				parser.parseTSV(tsv_string1)[4][0]);
+		assertEquals("Argentina",						parser.parseTSV(tsv_string1)[5][0]);
+		assertEquals("Drama",							parser.parseTSV(tsv_string1)[6][0]);
 	}
 
 }
