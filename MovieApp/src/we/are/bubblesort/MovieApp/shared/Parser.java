8e05d4280341d40d05d8ecf1ddf65aa860adb7c4
rafaelkallis <rafael@rafaelkallis.com>
2015-11-23 16:17:05 +0100

    updated parser

--- MovieApp/src/we/are/bubblesort/MovieApp/shared/Parser.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/shared/Parser.java
@@ -1,6 +1,9 @@
 package we.are.bubblesort.MovieApp.shared;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
@@ -25,7 +28,7 @@ public class Parser {
 	 * @param line the line of text to be parsed
 	 * @returns String[] array of all values parsed
 	 */
-	public String[] parseTSV(String line) throws IOException{
+	public String[][] parseTSV(String line) throws IOException{
 		return parseFormat(line,CSVFormat.TDF);
 	}
 	
@@ -33,7 +36,7 @@ public class Parser {
 	 * @param line the line of text to be parsed
 	 * @returns String[] array of all values parsed
 	 */
-	public String[] parseCSV(String line) throws IOException{
+	public String[][] parseCSV(String line) throws IOException{
 		return parseFormat(line,CSVFormat.DEFAULT);
 	}
 	
@@ -41,7 +44,7 @@ public class Parser {
 	 * @param line the line of text to be parsed
 	 * @returns String[] array of all values parsed
 	 */
-	public String[] parseExcel(String line) throws IOException{
+	public String[][] parseExcel(String line) throws IOException{
 		return parseFormat(line,CSVFormat.EXCEL);
 	}
 	
@@ -50,17 +53,34 @@ public class Parser {
 	 * @param format the format of the tring to be parsed. Used to specify the delimiter
 	 * @returns String[] array of all values parsed
 	 */
-	public String[] parseFormat(String line,CSVFormat format) throws IOException{
-		CSVRecord movieRecord 	= CSVParser.parse(line, format).getRecords().get(0);
-		String records[] 		= new String[7];
+	public String[][] parseFormat(String content,CSVFormat format) throws IOException{
+		List<CSVRecord> movieRecords = CSVParser.parse(content, format).getRecords();
+		String records[][] = new String[7][movieRecords.size()];
+		Iterator<CSVRecord> iterator = movieRecords.iterator();
+		int i =0;
+		while(iterator.hasNext()){
+			CSVRecord record = iterator.next();
+			records[0][i] 				= this.extractTitle		(record.get(2));
+			records[1][i] 				= this.extractYear		(record.get(3));
+			records[2][i] 				= this.extractRevenue	(record.get(4));
+			records[3][i] 				= this.extractDuration	(record.get(5));
+			records[4][i] 				= this.extractLanguages	(record.get(6));
+			records[5][i]				= this.extractCountries	(record.get(7));
+			records[6][i++] 			= this.extractGenres	(record.get(8));
+		}
 		
-		records[0] 				= this.extractTitle		(movieRecord.get(2));
-		records[1] 				= this.extractYear		(movieRecord.get(3));
-		records[2] 				= this.extractRevenue	(movieRecord.get(4));
-		records[3] 				= this.extractDuration	(movieRecord.get(5));
-		records[4] 				= this.extractLanguages	(movieRecord.get(6));
-		records[5]				= this.extractCountries	(movieRecord.get(7));
-		records[6] 				= this.extractGenres	(movieRecord.get(8));
+		
+		
+//		CSVRecord movieRecord 	= CSVParser.parse(line, format).getRecords().get(0);
+//		String records[] 		= new String[7];
+//		
+//		records[0] 				= this.extractTitle		(movieRecord.get(2));
+//		records[1] 				= this.extractYear		(movieRecord.get(3));
+//		records[2] 				= this.extractRevenue	(movieRecord.get(4));
+//		records[3] 				= this.extractDuration	(movieRecord.get(5));
+//		records[4] 				= this.extractLanguages	(movieRecord.get(6));
+//		records[5]				= this.extractCountries	(movieRecord.get(7));
+//		records[6] 				= this.extractGenres	(movieRecord.get(8));
 		
 		return records;
 	}
