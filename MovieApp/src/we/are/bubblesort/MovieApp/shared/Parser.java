8d0528b9409872acc988296608cc0e2303194314
rafaelkallis <rafael@rafaelkallis.com>
2015-11-23 20:09:36 +0100

    clean up

--- MovieApp/src/we/are/bubblesort/MovieApp/shared/Parser.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/shared/Parser.java
@@ -28,24 +28,24 @@ public class Parser {
 	 * @param line the line of text to be parsed
 	 * @returns String[] array of all values parsed
 	 */
-	public String[][] parseTSV(String line) throws IOException{
-		return parseFormat(line,CSVFormat.TDF);
+	public String[][] parseTSV(String content) throws IOException{
+		return parseFormat(content,CSVFormat.TDF);
 	}
 	
 	/*
 	 * @param line the line of text to be parsed
 	 * @returns String[] array of all values parsed
 	 */
-	public String[][] parseCSV(String line) throws IOException{
-		return parseFormat(line,CSVFormat.DEFAULT);
+	public String[][] parseCSV(String content) throws IOException{
+		return parseFormat(content,CSVFormat.DEFAULT);
 	}
 	
 	/*
 	 * @param line the line of text to be parsed
 	 * @returns String[] array of all values parsed
 	 */
-	public String[][] parseExcel(String line) throws IOException{
-		return parseFormat(line,CSVFormat.EXCEL);
+	public String[][] parseExcel(String content) throws IOException{
+		return parseFormat(content,CSVFormat.EXCEL);
 	}
 	
 	/*
@@ -57,8 +57,7 @@ public class Parser {
 		List<CSVRecord> movieRecords = CSVParser.parse(content, format).getRecords();
 		String records[][] = new String[7][movieRecords.size()];
 		Iterator<CSVRecord> iterator = movieRecords.iterator();
-		int i =0;
-		while(iterator.hasNext()){
+		for(int i = 0;iterator.hasNext();i++){
 			CSVRecord record = iterator.next();
 			records[0][i] 				= this.extractTitle		(record.get(2));
 			records[1][i] 				= this.extractYear		(record.get(3));
@@ -66,21 +65,8 @@ public class Parser {
 			records[3][i] 				= this.extractDuration	(record.get(5));
 			records[4][i] 				= this.extractLanguages	(record.get(6));
 			records[5][i]				= this.extractCountries	(record.get(7));
-			records[6][i++] 			= this.extractGenres	(record.get(8));
+			records[6][i]	 			= this.extractGenres	(record.get(8));
 		}
-		
-		
-		
-//		CSVRecord movieRecord 	= CSVParser.parse(line, format).getRecords().get(0);
-//		String records[] 		= new String[7];
-//		
-//		records[0] 				= this.extractTitle		(movieRecord.get(2));
-//		records[1] 				= this.extractYear		(movieRecord.get(3));
-//		records[2] 				= this.extractRevenue	(movieRecord.get(4));
-//		records[3] 				= this.extractDuration	(movieRecord.get(5));
-//		records[4] 				= this.extractLanguages	(movieRecord.get(6));
-//		records[5]				= this.extractCountries	(movieRecord.get(7));
-//		records[6] 				= this.extractGenres	(movieRecord.get(8));
 		
 		return records;
 	}
