b4b871e86e8ecf575c14c981f281c421b6e5adb0
rafaelkallis <rafael@rafaelkallis.com>
2015-11-24 14:07:56 +0100

    Added Wikipedia_id,Freebase_id,date

--- MovieApp/src/we/are/bubblesort/MovieApp/shared/MovieImportDAO.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/shared/MovieImportDAO.java
@@ -6,24 +6,22 @@ import org.apache.commons.csv.CSVFormat;
 
 public class MovieImportDAO {
 	String records[][];
-	
+	int n_movies;
 	public MovieImportDAO(String content,CSVFormat format) throws IOException{
-		String[][] temp_records = Parser.getInstance().parseFormat(content, format);
-		this.records = new String[8][temp_records[0].length];
-		this.records[1] = temp_records[0];
-		this.records[2] = temp_records[1];
-		this.records[3] = temp_records[2];
-		this.records[4] = temp_records[3];
-		this.records[5] = temp_records[4];
-		this.records[6] = temp_records[5];
-		this.records[7] = temp_records[6];
-	}
-	
-	/*
-	 * @pre records.length == 8
-	 */
-	public MovieImportDAO(String[][] records){
-		this.records = records;
+		String [][] temp_records = Parser.getInstance().parseFormat(content, format);
+		this.n_movies = temp_records[0].length;
+		this.records = new String[11][this.n_movies];
+		
+		this.records[1] 	= temp_records[0]; //WikipediaIDs
+		this.records[2] 	= temp_records[1]; //FreebaseId
+		this.records[3] 	= temp_records[2]; //Title
+		this.records[4] 	= temp_records[3]; //Date
+		this.records[5] 	= temp_records[4]; //Year
+		this.records[6] 	= temp_records[5]; //Revenue
+		this.records[7] 	= temp_records[6]; //Duration
+		this.records[8] 	= temp_records[7]; //Languages
+		this.records[9] 	= temp_records[8]; //Countries
+		this.records[10] 	= temp_records[9]; //Genres
 	}
 	
 	/*
@@ -37,58 +35,47 @@ public class MovieImportDAO {
 		return records[0];
 	}
 	
-	public String[] getTitles(){
+	public String[] getWikipediaIds(){
 		return records[1];
 	}
 	
-	public String[] getYears(){
+	public String[] getFreebaseIds(){
 		return records[2];
 	}
 	
-	public String[] getRevenues(){
+	public String[] getTitles(){
 		return records[3];
 	}
 	
-	public String[] getDurations(){
+	public String[] getDates(){
 		return records[4];
 	}
 	
-	public String[] getLanguages(){
+	public String[] getYears(){
 		return records[5];
 	}
 	
-	public String[] getCountries(){
+	public String[] getRevenues(){
 		return records[6];
 	}
-		
-	public String[] getGenres(){
+	
+	public String[] getDurations(){
 		return records[7];
 	}
 	
-	public int getNMovies(){
-		return records[0].length;
+	public String[] getLanguages(){
+		return records[8];
 	}
 	
-	public String[] getAttributes(String dbLabelName){
-		switch(dbLabelName){
-			case MovieID.dbLabelName:
-				return this.getIDs();
-			case MovieTitle.dbLabelName:
-				return this.getTitles();
-			case MovieYear.dbLabelName:
-				return this.getYears();
-			case MovieRevenue.dbLabelName:
-				return this.getRevenues();
-			case MovieDuration.dbLabelName:
-				return this.getDurations();
-			case MovieLanguage.dbLabelName:
-				return this.getLanguages();
-			case MovieCountry.dbLabelName:
-				return this.getCountries();
-			case MovieGenre.dbLabelName:
-				return this.getGenres();
-			default:
-				return null;
-		}
+	public String[] getCountries(){
+		return records[9];
+	}
+		
+	public String[] getGenres(){
+		return records[10];
+	}
+	
+	public int getNMovies(){
+		return this.n_movies;
 	}
 }
