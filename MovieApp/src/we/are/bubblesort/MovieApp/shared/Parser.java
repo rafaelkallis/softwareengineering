e9b69dbdd9d35a8d022e37eb791dc85ecc370f94
rafaelkallis <rafael@rafaelkallis.com>
2015-11-25 20:56:19 +0100

    Added quote ignore on parsing

--- MovieApp/src/we/are/bubblesort/MovieApp/shared/Parser.java
+++ MovieApp/src/we/are/bubblesort/MovieApp/shared/Parser.java
@@ -3,7 +3,6 @@ package we.are.bubblesort.MovieApp.shared;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
-
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
@@ -29,13 +28,14 @@ public class Parser {
 	 * @returns String[] array of all values parsed
 	 */
 	public String[][] parseTSV(String content) throws IOException{
-		return parseFormat(content,CSVFormat.TDF);
+		return parseFormat(content,CSVFormat.TDF.withQuote(null));
 	}
 	
 	/*
 	 * @param line the line of text to be parsed
 	 * @returns String[] array of all values parsed
 	 */
+	@Deprecated
 	public String[][] parseCSV(String content) throws IOException{
 		return parseFormat(content,CSVFormat.DEFAULT);
 	}
@@ -44,6 +44,7 @@ public class Parser {
 	 * @param line the line of text to be parsed
 	 * @returns String[] array of all values parsed
 	 */
+	@Deprecated
 	public String[][] parseExcel(String content) throws IOException{
 		return parseFormat(content,CSVFormat.EXCEL);
 	}
@@ -54,22 +55,22 @@ public class Parser {
 	 * @returns String[] array of all values parsed
 	 */
 	public String[][] parseFormat(String content,CSVFormat format) throws IOException{
-		List<CSVRecord> movieRecords = CSVParser.parse(content, format).getRecords();
+		List<CSVRecord> movieRecords = CSVParser.parse((content), format).getRecords();
 		String records[][] = new String[10][movieRecords.size()];
 		Iterator<CSVRecord> iterator = movieRecords.iterator();
 		for(int i = 0;iterator.hasNext();i++){
 			CSVRecord record = iterator.next();
 			
-			records[0][i] 				= this.extractWikipediaId	(record.get(0));
-			records[1][i] 				= this.extractFreebaseId	(record.get(1));
-			records[2][i] 				= this.extractTitle			(record.get(2));
-			records[3][i] 				= this.extractDate			(record.get(3));
-			records[4][i] 				= this.extractYear			(record.get(3));
-			records[5][i] 				= this.extractRevenue		(record.get(4));
-			records[6][i] 				= this.extractDuration		(record.get(5));
-			records[7][i] 				= this.extractLanguages		(record.get(6));
-			records[8][i]				= this.extractCountries		(record.get(7));
-			records[9][i]	 			= this.extractGenres		(record.get(8));
+			records[0][i] 				= this.extractWikipediaId	((record.get(0)));
+			records[1][i] 				= this.extractFreebaseId	((record.get(1)));
+			records[2][i] 				= this.extractTitle			((record.get(2)));
+			records[3][i] 				= this.extractDate			((record.get(3)));
+			records[4][i] 				= this.extractYear			((record.get(3)));
+			records[5][i] 				= this.extractRevenue		((record.get(4)));
+			records[6][i] 				= this.extractDuration		((record.get(5)));
+			records[7][i] 				= this.extractLanguages		((record.get(6)));
+			records[8][i]				= this.extractCountries		((record.get(7)));
+			records[9][i]	 			= this.extractGenres		((record.get(8)));
 		}
 		
 		return records;
