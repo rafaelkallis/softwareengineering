d2fe94471799ca5dfaa19adaed3afabb63bc769d
rafaelkallis <rafael@rafaelkallis.com>
2015-11-25 21:06:07 +0100

    updated tests

--- MovieApp/test/we/are/bubblesort/MovieApp/shared/ParserTest.java
+++ MovieApp/test/we/are/bubblesort/MovieApp/shared/ParserTest.java
@@ -37,5 +37,21 @@ public class ParserTest {
 		assertEquals("Argentina",						parsed1[8][0].toString());
 		assertEquals("Drama",							parsed1[9][0].toString());
 	}
+	
+	@Test
+	public void test() throws IOException{
+		String tsv_string0 = "31138563	/m/0gh76d7	\"A Royal\" Divorce	1926			{}	{}	{\"/m/03g3w\": \"History\", \"/m/07s9rl0\": \"Drama\"}";
+		String[][] parsed0 = parser.parseTSV(tsv_string0);
+		assertEquals("31138563",						parsed0[0][0].toString());
+		assertEquals("/m/0gh76d7",						parsed0[1][0].toString());
+		assertEquals("\"A Royal\" Divorce",				parsed0[2][0].toString());
+		assertEquals("1926",							parsed0[3][0].toString());
+		assertEquals("1926",							parsed0[4][0].toString());
+		assertEquals("",								parsed0[5][0].toString());
+		assertEquals("",								parsed0[6][0].toString());
+		assertEquals("",								parsed0[7][0].toString());
+		assertEquals("",								parsed0[8][0].toString());
+		assertEquals("History, Drama",					parsed0[9][0].toString());
+	}
 
 }
