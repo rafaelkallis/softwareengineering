7ed2dc52a81a8c3340ea055f7e9f6623242f49a2
rafaelkallis <rafael@rafaelkallis.com>
2015-12-04 13:00:19 +0100

    clean up

--- MovieApp/test/we/are/bubblesort/MovieApp/server/MovieImportDAOTest.java
+++ MovieApp/test/we/are/bubblesort/MovieApp/server/MovieImportDAOTest.java
@@ -12,7 +12,7 @@ import we.are.bubblesort.MovieApp.shared.ImportFormatException;
 public class MovieImportDAOTest {
 
 	@Test(expected=ImportFormatException.class)
-	public void testParse() throws ImportFormatException, IOException{
+	public void testUnsupportedImport() throws ImportFormatException, IOException{
 		String content = "975900,/m/03vyhn,Ghosts of Mars,2001-08-24,14010832,98.0,{\"/m/02h40lc\": \"English Language\"},{\"/m/09c7w0\": \"United States of America\"},{\"/m/01jfsb\": \"Thriller\", \"/m/06n90\": \"Science Fiction\", \"/m/03npn\": \"Horror\", \"/m/03k9fj\": \"Adventure\", \"/m/0fdjb\": \"Supernatural\", \"/m/02kdv5l\": \"Action\", \"/m/09zvmj\": \"Space western\"}";	
 		@SuppressWarnings("unused")
 		MovieImportDAO dao = new MovieImportDAO(content);
